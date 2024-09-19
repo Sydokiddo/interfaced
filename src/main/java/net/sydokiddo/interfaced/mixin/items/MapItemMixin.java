427cca3ad2f8458eabf8153401e579c94dd4434f
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2024-09-18 22:51:02 -0700

    Updated to Chrysalis v0.4.2

---
+++ src/main/java/net/sydokiddo/interfaced/mixin/items/MapItemMixin.java
@@ -0,0 +1,26 @@
+package net.sydokiddo.interfaced.mixin.items;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.MapItem;
+import net.sydokiddo.interfaced.registry.misc.util.MapTooltipComponent;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import java.util.Optional;
+
+@Mixin(MapItem.class)
+public class MapItemMixin extends Item {
+
+    private MapItemMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Environment(EnvType.CLIENT)
+    @Override
+    public @NotNull Optional<TooltipComponent> getTooltipImage(@NotNull ItemStack itemStack) {
+        return Optional.of(new MapTooltipComponent(itemStack));
+    }
+}
--- src/main/java/net/sydokiddo/interfaced/registry/misc/ModTags.java
+++ src/main/java/net/sydokiddo/interfaced/registry/misc/ModTags.java
@@ -7,17 +7,21 @@ import net.sydokiddo.interfaced.Interfaced;
 
 public class ModTags {
 
-    // Tags
+    // region Tags
 
     public static final TagKey<Item>
         FOOD_TOOLTIP_BLACKLISTED = registerItemTag("food_tooltip_blacklisted"),
         TOOLTIP_SPACE_BLACKLISTED = registerItemTag("tooltip_space_blacklisted")
     ;
 
-    // Registry
+    // endregion
+
+    // region Registry
 
     @SuppressWarnings("all")
     private static TagKey<Item> registerItemTag(String name) {
         return TagKey.create(Registries.ITEM, Interfaced.id(name));
     }
+
+    // endregion
 }
