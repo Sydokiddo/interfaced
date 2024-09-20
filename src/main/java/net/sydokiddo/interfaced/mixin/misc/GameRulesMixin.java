e02f98f6f0dcf6a053bced2f58c240a21a06e796
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2024-09-19 17:16:58 -0700

    Changed the default gamerule value of reduceDebugInfo

---
+++ src/main/java/net/sydokiddo/interfaced/mixin/misc/GameRulesMixin.java
@@ -0,0 +1,19 @@
+package net.sydokiddo.interfaced.mixin.misc;
+
+import net.minecraft.world.level.GameRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRules.class)
+public abstract class GameRulesMixin {
+
+    @Shadow public abstract <T extends GameRules.Value<T>> T getRule(GameRules.Key<T> key);
+
+    @Inject(method = "<init>()V", at = @At("TAIL"))
+    private void interfaced$changeDefaultGameruleValues(CallbackInfo info) {
+        this.getRule(GameRules.RULE_REDUCEDDEBUGINFO).set(true, null);
+    }
+}
